6e9c261e3a57bc4f2adeff078a47061b121a6581
Muhammad Towhid <1705017@ugrad.cse.buet.ac.bd>
2022-07-10 02:43:35 +0600

    f

---
+++ E-Learners_source_code/frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css"
+// import "react-datepicker/dist/react-datepicker.css";
+
+import Topbar from "./components/topbar";
+
+import Login from "./pages/login";
+import Admins from "./pages/admins";
+
+
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
+// import {useEffct} from 'react';
+
+function App() {
+
+  return (
+    <div className="fintech">
+      <Topbar/>
+      <div className="appcontainer">
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/admins" element={<Admins/>}/>
+        </Routes>
+       
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
