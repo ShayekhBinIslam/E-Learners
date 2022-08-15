64f94be3240dcfa8e9448c02fff4a2ddc26daf01
ShayekhBinIslam <shikh.bin.islam@gmail.com>
2022-08-15 10:17:41 +0600

    add freeslot page added

--- E-Learners_source_code/frontend/src/pages/Freetime.js
+++ E-Learners_source_code/frontend/src/pages/Freetime.js
@@ -4,6 +4,16 @@ export default function Freetime() {
   
   return (
     <div>
+      <h1> Enter Freetime: </h1>
+      <div className="form-group">
+        {/* Inpute date */}
+        <label for="start_date">Start Date</label>
+        <input type="date" name="start_date"/>
+        <label for="end_date">End Date</label>
+        <input type="date" name="end_date"/>
+        <button type="submit" className="btn btn-primary">Add Free Slot</button>
+
+      </div>
     </div>
   )
 
