9bb21726e0105900c0f4f3c590a667dcc5d06de1
ShayekhBinIslam <shikh.bin.islam@gmail.com>
2022-08-15 11:40:44 +0600

    slot backend data received

--- E-Learners_source_code/frontend/src/pages/Freetime.js
+++ E-Learners_source_code/frontend/src/pages/Freetime.js
@@ -5,6 +5,8 @@ import moment from "moment";
 export default function Freetime() {
   const [startDate, setStartDate] = useState(moment().format("YYYY-MM-DD"));
   const [endDate, setEndDate] = useState(moment().format("YYYY-MM-DD"));
+  const [startTime, setStartTime] = useState(moment().format("00:00"));
+  const [endTime, setEndTime] = useState(moment().format("00:00"));
   console.log(startDate)
   console.log(endDate)
 
@@ -12,24 +14,33 @@ export default function Freetime() {
     console.log(startDate)
     console.log(endDate)
     let formField = new FormData()
-    // formField.append('start_date', startDate)
-    // formField.append('end_date', endDate)
+    formField.append('start_date', startDate)
+    formField.append('end_date', endDate)
+    formField.append('start_time', startTime)
+    formField.append('end_time', endTime)
     
-    formField.append('user', localStorage.getItem('user_id'))
+    formField.append('user_id', localStorage.getItem('user_id'))
+    // formField.append('a', 'a');
 
-    console.log(JSON.stringify(formField))
+    // console.log(JSON.stringify(formField))
+    // console.log(formField.entries())
 
-    // var userid = localStorage.getItem('user_id')
-    // console.log(userid)
-    // let data;
-    // axios.post('http://localhost:8000/add_freeslot/', {'user': userid})
-    //   .then(res=>{
-    //     data = res.data;
-    //     // this.setState({
-    //     //   details: data
-    //     // });
-    //   })
-    //   .catch(err=>{})
+    for (var key of formField.entries())
+    {
+      console.log(key[0] + ', ' + key[1]);
+    }
+
+    var userid = localStorage.getItem('user_id')
+    console.log(userid)
+    let data;
+    await axios({
+      method: 'post',
+      url:'http://localhost:8000/add_freeslot/',
+      data: formField
+    }).then(response=>{
+      console.log(response.data);
+     // history.push('/admins')
+    })
   }
 
   return (
@@ -37,19 +48,32 @@ export default function Freetime() {
       <h1> Enter Freetime: </h1>
       <div className="form-group">
         {/* Inpute date */}
-        <label for="start_date">Start Date</label>
+        <label for="start_date">Start</label>
         <input type="date" 
           name="start_date" 
           value={startDate}
           // onLoad={(e) => setStartDate(e.target.value)}
           onChange={(e) => setStartDate(e.target.value)}/>
+        
+        <label for="start_time"></label>
+        <input type="time" 
+          name="start_time" 
+          value={startTime}
+          // onLoad={(e) => setStartDate(e.target.value)}
+          onChange={(e) => setStartTime(e.target.value)}/>
 
-        <label for="end_date">End Date</label>
+        <label for="end_date">End</label>
         <input type="date" 
           name="end_date"
           value={endDate}
           // onLoad={(e) => setStartDate(e.target.value)}
           onChange={(e) => setEndDate(e.target.value)}/>
+        <label for="end_time"></label>
+        <input type="time" 
+          name="end_time" 
+          value={endTime}
+          // onLoad={(e) => setStartDate(e.target.value)}
+          onChange={(e) => setEndTime(e.target.value)}/>
 
         <button type="submit" className="btn btn-primary" onClick={addFreeslot}>
           Add Free Slot
